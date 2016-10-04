40c652f5e85c986c66cc7e5728e6c5f373c90e5b
Richurd <richard.odell@hotmail.com>
2016-10-04 19:00:24 +0200

    Richurd

--- main.py
+++ main.py
@@ -15,19 +15,26 @@ def buildPMatrix(data): #Calculates and returns the matrix P
 		rowVector = []
 		point = data[i]
 		t1 = point[2]
-		x1 = point[0]
+		x1 = [point[0], point[1]]
 		for j in range(len(data)): #Go through the columns
 			point = data[j]
 			t2 = point[2]
-			x2 = point[1]
+			x2 = [point[0], point[1]]
 			newValue = t1 * t2 * linearKernel(x1,x2)
 			rowVector.append(newValue)
 		P.append(rowVector)
 	return P
 
 
-def buildqGh(): # Next step
-	pass
+def buildqGh(N): # Next step
+	q = []; G = []; h = []
+	for i in range(N):
+		q.append(float(-1))
+		G = -1*numpy.identity(N, float)
+		h.append(float(0))
+	return q, G, h
+
+
 
 
 # Uppgift 4
@@ -37,10 +44,21 @@ classA = [(random.normalvariate(-1.5, 1), random.normalvariate(0.5, 1), 1.0) for
 classB = [(random.normalvariate(0.0, 0.5), random.normalvariate( -0.5, 0.5 ), -1.0) for i in range(10)]
 data = classA + classB
 random.shuffle(data)
-
+print(data)
 
 # test
+def main():
+	data = [(1.,2.,1.), (0.,0.,-1.), (0.9, 2.2, 1), (0., 0.1, -1.), (1.1,2.01,1.), (0.02,0.1,-1.), (0.95, 2.25, 1), (0.03, 0.1, -1.)]
+	P = buildPMatrix(data)
+	q, G, h = buildqGh(len(data))
+	print('\n\n\n', q, '\n\n\n', G,'\n\n\n', h)
 
-#P = buildPMatrix(data)
-P = buildPMatrix([(2,5,1),(3,4,-1)])
-print(P)
+	r = qp(matrix(P), matrix(q), matrix(G), matrix(h))
+	alpha = list(r['x'])
+	print(alpha)
+
+
+	return
+
+
+main()
